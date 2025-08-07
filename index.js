df082ade62530aa3e5920067cf063d7892985dd3
1afzal <afzalali5256@gmail.com>
2025-08-08 00:11:35 +0530

    greener the better

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-import jsonfile from "jsonfile";
-import moment from "moment";
-import simpleGit from "simple-git";
-import random from "random";
+const makeCommits = (n, commitsPerDay = 3) => {
+  if (n === 0) return simpleGit().push();
 
-const path = "./data.json";
-
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
-};
-
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 40);
-  const y = random.int(0, 6);
+  const x = random.int(0, 40); // random week
+  const y = random.int(0, 6);  // random day of the week
   const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
 
   const data = {
     date: date,
   };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
+
+  const git = simpleGit();
+
+  // Recursive function to make multiple commits on the same day
+  const makeMultipleCommits = (count) => {
+    if (count === 0) {
+      return makeCommits(--n, commitsPerDay); // Move to the next day
+    }
+
+    jsonfile.writeFile(path, data, () => {
+      git.add([path]).commit(date, { "--date": date }, () => {
+        makeMultipleCommits(count - 1); // Commit again on same date
+      });
+    });
+  };
+
+  console.log(`Making ${commitsPerDay} commits on ${date}`);
+  makeMultipleCommits(commitsPerDay); // Start multiple commits on same day
 };
 
-makeCommits(100);
+// Call with desired total days and commits per day
+makeCommits(100, 5); // 100 days, 5 commits per day
